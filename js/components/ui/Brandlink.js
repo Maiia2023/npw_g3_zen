154c914aa1f41e56b9bf2487581b52fa2d04159c
Maya B <newmay4@gmail.com>
2023-06-25 00:24:47 +0300

    Changed file Brandlink.js

--- js/components/ui/Brandlink.js
+++ js/components/ui/Brandlink.js
@@ -1,22 +1,28 @@
 /**
- * @function BrandLink
  * @typedef {Object} link
  * @property {string} url
  * @property {string} type
+ */
+
+/**
+ * @function BrandLink
  * @param {link} link 
  * @param {string} className 
  * @returns {string} HTML or empty
  */
 
 export const BrandLink = (link, className) => {
-  if (!link) return '';
+  if (Object.values(link).length !== 2) return '';
+
+  const {url, type} = link;
+
   return`
-  <a
-    href="${link.url}"
-    class="${className}"
-  >
-      ${link.type === 'apple' && IconApple ()};
-      ${link.type === 'google' && IconGoogle ()};
-  </a>
+    <a
+      href="${url}"
+      class="${className}"
+    >
+      ${type === 'apple' && IconApple ()};
+      ${type === 'google' && IconGoogle ()};
+    </a>
   `;
 };
