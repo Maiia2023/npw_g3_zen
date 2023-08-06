238875d5a7bc1241e8d75930e3ce1138b25dc881
Maya B <newmay4@gmail.com>
2023-08-06 11:45:43 +0300

    Added russian version to the mock data

--- js/data.js
+++ js/data.js
@@ -260,5 +260,265 @@ export const data = {
       },
     },
   },
-  ru: {},
+  ru: {
+    header: {
+      logo: {
+        source: 'assets/icons/logo.svg',
+        description: 'logo',
+      },
+      langs: ['en', 'ru'],
+      nav: [
+        {
+          target: 'download',
+          content: 'Скачать',
+          type: 'anchor',
+        },
+        {
+          target: 'warranty',
+          content: 'Гарантии',
+          type: 'anchor',
+        },
+        {
+          target: 'care',
+          content: 'Забота',
+          type: 'anchor',
+        },
+        {
+          target: 'cashback',
+          content:  'Кэшбэк',
+          type: 'anchor',
+        },
+        {
+          target: 'clients',
+          content: 'Клиенты',
+          type: 'anchor',
+        },
+      ]
+    },
+    download: {
+      name: 'download',
+      title: {
+        priority: 1,
+        content: 'Безопасные покупки с заботой о деньгах',
+      },
+      texts: [
+        'Узнайте, как мы можем помочь сделать ваш опыт покупок и управления деньгами более совершенным c ZEN.',
+      ],
+      image: {
+        source: 'assets/images/1.jpg',
+        description: 'описание к изображению 1',
+      },
+      links: {
+        apple: {
+          url: 'https://apple.com',
+        },
+        google: {
+          url: 'https://google.com',
+        },
+      },
+    },
+    warranty: {
+      name: 'warranty',
+      title: {
+        priority: 2,
+        content: 'Увеличение гарантии на 1 год',
+      },
+      texts: [
+        'Карты ZEN имеют очень удобную функцию, которая поможет вам, когда ваши электронные устройства начинают выходить из строя после окончания стандартной гарантии, что случается довольно часто.',
+        'При покупке любой электроники с помощью карты ZEN, гарантия на неё продлевается на один год. Как это работает? Просто. Автоматически.'
+      ],
+      image: {
+        source: 'assets/images/2.jpg',
+        description: 'описание к излбражению 2',
+      }
+    },
+    care: {
+      name: 'care',
+      title: {
+        priority: 2,
+        content: 'ZEN забота',
+      },
+      texts: [
+        'Покупки в Интернете обычно доставляют удовольствие. Но не всегда.',
+        'Купленная вами вещь может оказаться бракованной, или совершенно не той, которую вы покупаете, и вам предстоит долгая процедура возврата товара.',
+        'Но вот появляется ZEN с бесплатной встроенной защитой покупок.'
+      ],
+      image: {
+        source: 'assets/images/3.jpg',
+        description: 'описание к изображению 3',
+      },
+    },
+    cashback: {
+      name: 'cashback',
+      title: {
+        priority: 2,
+        content: 'Кэшбэк',
+      },
+      texts: [
+        'Мы помогаем сэкономить деньги, пока вы делаете покупки, ведь зачастую требуется целая вечность, чтобы получить хоть какую-то выгоду.',
+        'Мы подумали об этом и сделали cashback более дружелюбным.',
+        'Используя ZEN Mastercard ®, до 14% от того, что вы потратите, поступит на ваш счёт сразу после покупки.'
+      ],
+      buttonText: 'Заказать консультацию',
+    },
+    clients: {
+      name: 'clients',
+      title: {
+        priority: 2,
+        content: 'Клиенты',
+      },
+      brands: [
+        {
+          source: 'assets/clients/brand1.svg',
+          description: 'Rosegal',
+        },
+        {
+          source: 'assets/clients/brand2.svg',
+          description: 'Gearbest',
+        },
+        {
+          source: 'assets/clients/brand3.svg',
+          description: 'Allegro',
+        },
+        {
+          source: 'assets/clients/brand4.svg',
+          description: 'Aliexpress',
+        },
+        {
+          source: 'assets/clients/brand5.svg',
+          description: 'Booking.com',
+        },
+        {
+          source: 'assets/clients/brand6.svg',
+          description: 'MediaMarkt',
+        },
+        {
+          source: 'assets/clients/brand7.svg',
+          description: 'RTV',
+        },
+        {
+          source: 'assets/clients/brand8.svg',
+          description: 'Groupon',
+        },
+      ],
+    },
+    footer: {
+      columns: [
+        {
+          title: {
+            priority: 3,
+            content: 'Изучите',
+          },
+          links: [
+            {
+              type: 'text',
+              url: 'https//:example.com',
+              content: 'Центр помощи',
+            },
+            {
+              type: 'text',
+              url: 'https//:example.com',
+              content: 'Присоединяйтесь',
+            },
+            {
+              type: 'text',
+              url: 'https//:example.com',
+              content: 'Политика',
+            },
+            {
+              type: 'text',
+              url: 'https//:example.com',
+              content: 'Файлы cookie',
+            },
+            {
+              type: 'text',
+              url: 'https//:example.com',
+              content: 'Правила',
+            },
+          ]
+        },
+        {
+          title: {
+            priority: 3,
+            content: 'Откройте',
+          },
+          links: [
+            {
+              type: 'text',
+              url: 'htts//example.com',
+              content: 'Разработчикам',
+            },
+            {
+              type: 'text',
+              url: 'htts//example.com',
+              content: 'Apple Pay',
+            },
+            {
+              type: 'text',
+              url: 'htts//example.com',
+              content: 'Google Pay',
+            },
+          ]
+        },
+        {
+          title: {
+            priority: 3,
+            content: 'Предложение',
+          },
+          links: [
+            {
+              type: 'text',
+              url: 'https://example.com',
+              content: 'Бизнес',
+            },
+            {
+              type: 'text',
+              url: 'https://example.com',
+              content: 'Персональное',
+            },
+          ]
+        },
+        {
+          title: {
+            priority: 3,
+            content: 'Язык',
+          },
+          links: [
+            {
+              type: 'text',
+              url: 'https://example.com/',
+              content: 'Английский',
+            },
+            {
+              type: 'text',
+              url: 'https://example.com/',
+              content: 'Русский',
+            },
+          ]
+        },
+        {
+          title: {
+            priority: 3,
+            content: 'Следите',
+          },
+          link: {
+            type: 'in',
+            url: 'https://linkedin.com/',
+          },
+        },
+      ],
+      texts: [
+          'ZEN является лицензированным финансовым учреждением под надзором Центрального банка Литвы, утвержденным Европейским банковским управлением для 31 страны.',
+          'Регистрационный номер компании 304749651, идентификатор НДС LT100011714916.',
+          'Лицензия учреждений по приему электронных денежных средств № LB000457.',
+          'Уставной капитал 2.185.000,00 EUR.',
+          'Адрес компании: улица Мезиню, 5, LT-01133, Вильнюс, Литва.',
+          'Офис продаж: ул. Эмилия Платера 53, 00-113 Варшава, Польша.',
+      ],
+      developer: {
+        url: 'http://example.com',
+        type: 'dev'
+      },
+    },
+  },
 };
