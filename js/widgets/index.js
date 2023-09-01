f57831eaaa827f278dec475e31b6fc7fbaaea02d
Maya B <newmay4@gmail.com>
2023-09-01 14:50:04 +0300

    Added Order component
    Deleted import types from Order component
    
    Implemented ModalOrder component
    
    Added Order component
    
    Added ModalOrder to exports file
    
    Added Order component to exports file
    
    Rearrange file with styles
    
    Fixed class for ModalOrder

--- js/widgets/index.js
+++ js/widgets/index.js
@@ -2,3 +2,4 @@ export { Header } from './Header/Header.js';
 export { SectionPrimary } from './SectionPrimary/SectionPrimary.js';
 export { SectionSecondary } from './SectionSecondary/SectionSecondary.js';
 export { SectionTertiary } from './SectionTertiary/SectionTertiary.js';
+export { Order } from './Order/Order.js';
